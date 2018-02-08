14e206b3aba74536e308c2da9a40a57963f4d9f9
Trung Le <trung.t.le.nguyen@gmail.com>
2018-02-08 10:17:07 -0800

    Initial commit

---
+++ js/cbsaScripts.js
@@ -0,0 +1,105 @@
+/* eslint-env jquery */
+
+$(".mSelectAll").click(function() {
+	var a = $("input[type='checkbox'].mChild");
+	if($(this).is(":checked")) {
+		a.prop("checked", true);
+	}
+	else {
+		a.prop("checked", false);
+	}
+});
+
+$("input[type='checkbox'].mChild").change(function() {
+	var a = $("input[type='checkbox'].mChild");
+	if(a.length == a.filter(":checked").length) {
+		$(".mSelectAll").prop("checked", true);
+	}
+	else {
+		$(".mSelectAll").prop("checked", false);
+	}
+});
+
+$(".rSelectAll").click(function() {
+	var a = $("input[type='checkbox'].rChild");
+	if($(this).is(":checked")) {
+		a.prop("checked", true);
+	}
+	else {
+		a.prop("checked", false);
+	}
+});
+
+$("input[type='checkbox'].rChild").change(function() {
+	var a = $("input[type='checkbox'].rChild");
+	if(a.length == a.filter(":checked").length) {
+		$(".rSelectAll").prop("checked", true);
+	}
+	else {
+		$(".rSelectAll").prop("checked", false);
+	}
+});
+
+$(".pSelectAll").click(function() {
+	var a = $("input[type='checkbox'].pChild");
+	if($(this).is(":checked")) {
+		a.prop("checked", true);
+	}
+	else {
+		a.prop("checked", false);
+	}
+});
+
+$("input[type='checkbox'].pChild").change(function() {
+	var a = $("input[type='checkbox'].pChild");
+	if(a.length == a.filter(":checked").length) {
+		$(".pSelectAll").prop("checked", true);
+	}
+	else {
+		$(".pSelectAll").prop("checked", false);
+	}
+});
+
+$(".sSelectAll").click(function() {
+	var a = $("input[type='checkbox'].sChild");
+	if($(this).is(":checked")) {
+		a.prop("checked", true);
+	}
+	else {
+		a.prop("checked", false);
+	}
+});
+
+$("input[type='checkbox'].sChild").change(function() {
+	var a = $("input[type='checkbox'].sChild");
+	if(a.length == a.filter(":checked").length) {
+		$(".sSelectAll").prop("checked", true);
+	}
+	else {
+		$(".sSelectAll").prop("checked", false);
+	}
+});
+
+$(document).ready(function(){
+	var a = $("input[name='master']");
+	var b = $("input[name='cbsaChild']");
+
+	a.on("change", function() {
+		var string = b.filter(":checked").map(function(){
+			return this.value;
+		}).get().join("|");
+		$("#output").val(string);
+	});
+    
+	b.on("change", function() {
+		var string = b.filter(":checked").map(function(){
+			return this.value;
+		}).get().join("|");
+		$("#output").val(string);
+	});
+});
+
+$("button").click(function() {
+	$("textarea").select();
+	document.execCommand("copy");
+});
